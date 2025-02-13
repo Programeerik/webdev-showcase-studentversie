f65220fd954ca1e14f807ab1ddfb1cb3e0e2679a
programeerik <erik.leusink22@gmail.com>
2025-02-13 16:21:03 +0100

    Readded gdpr and added Watch to docker-compose file.

--- ShowcaseProject/ShowcaseFrontend/Controllers/HomeController.cs
+++ ShowcaseProject/ShowcaseFrontend/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace Showcase_Contactpagina.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
